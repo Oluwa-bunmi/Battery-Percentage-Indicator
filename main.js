e294ee00e751c89ed2638d15668ddd1befce17ec
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-20 23:00:22 +0100

    declaring variables in js

---
+++ main.js
@@ -0,0 +1,8 @@
+initBattery()
+function initBattery(){
+    const batteryLiquid = document.querySelector("batteryLiquid");
+    const batteryStatus = document.querySelector(".status");
+    const batteryPercent = document.querySelector("<h1>");
+
+    
+}
