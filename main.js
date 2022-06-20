2d98ae03ca4dcbc5c0a06b0726185cc92055da28
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-20 23:12:44 +0100

    building

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ function initBattery(){
 
     navigator.getBattery().then((batt) => {
         updateBattery = () => {
-// Updating the number level of battery
+          // Updating the number level of battery
+          let level = Math.floor(batt.level * 100);
+          batteryPercent.innerHTML = level + " %";
+          // Updating background level of battery
+          batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
         }
+       
         updateBattery()
     })
 }
