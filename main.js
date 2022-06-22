4693e73f0f2b896d4c92d6ae1c82a4a1b71f3979
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-23 00:49:23 +0100

    problem solved

--- main.js
+++ main.js
@@ -12,56 +12,56 @@ function initBattery() {
       // Updating background level of battery
       batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
       // Validating full and low battery and if it's not charging
-      // if (level == 100) {
-      //   batteryStatus.innerHTML = `Full battery <i class="fa-solid fa-battery-full green "></i>`;
-      //   batteryLiquid.style.height = "103%"; // Hiding the ellipse
-      // } else if ((level <= 20) &! batt.charging) {
-      //   batteryStatus.innerHTML = `Low battery <i class="fa-solid fa-plug animated-red"></i>`;
-      // } else if (batt.charging) {
-      //   batteryStatus.innerHTML = `Charging... <i class="fa-solid fa-bolt-lightning animated-green"></i>`;
-      // } else {
-      //   batteryStatus.innerHTML = "";
-      // }
-      // // Changing battery color and removing other colors
-      // if (level <= 20) {
-      //   batteryLiquid.classList.add("gradientRed");
-      //   batteryLiquid.classList.remove(
-      //     "gradientGreen",
-      //     "gradientOrange",
-      //     "gradientYellow"
-      //   );
-      // } else if (level <= 40) {
-      //   batteryLiquid.classList.add("gradientOrange");
-      //   batteryLiquid.classList.remove(
-      //     "gradientGreen",
-      //     "gradientRed",
-      //     "gradientYellow"
-      //   );
-      // } else if (level <= 80) {
-      //   batteryLiquid.classList.add("gradientYellow");
-      //   batteryLiquid.classList.remove(
-      //     "gradientGreen",
-      //     "gradientRed",
-      //     "gradientOrange"
-      //   );
-      // } else {
-      //   batteryLiquid.classList.add("gradientGreen");
-      //   batteryLiquid.classList.remove(
-      //     "gradientYellow",
-      //     "gradientRed",
-      //     "gradientOrange"
-      //   );
-      // }
+      if (level == 100) {
+        batteryStatus.innerHTML = `Full battery <i class="fa-solid fa-battery-full green "></i>`;
+        batteryLiquid.style.height = "103%"; // Hiding the ellipse
+      } else if ((level <= 20) &! batt.charging) {
+        batteryStatus.innerHTML = `Low battery <i class="fa-solid fa-plug animated-red"></i>`;
+      } else if (batt.charging) {
+        batteryStatus.innerHTML = `Charging... <i class="fa-solid fa-bolt-lightning animated-green"></i>`;
+      } else {
+        batteryStatus.innerHTML = "";
+      }
+      // Changing battery color and removing other colors
+      if (level <= 20) {
+        batteryLiquid.classList.add("gradientRed");
+        batteryLiquid.classList.remove(
+          "gradientGreen",
+          "gradientOrange",
+          "gradientYellow"
+        );
+      } else if (level <= 40) {
+        batteryLiquid.classList.add("gradientOrange");
+        batteryLiquid.classList.remove(
+          "gradientGreen",
+          "gradientRed",
+          "gradientYellow"
+        );
+      } else if (level <= 80) {
+        batteryLiquid.classList.add("gradientYellow");
+        batteryLiquid.classList.remove(
+          "gradientGreen",
+          "gradientRed",
+          "gradientOrange"
+        );
+      } else {
+        batteryLiquid.classList.add("gradientGreen");
+        batteryLiquid.classList.remove(
+          "gradientYellow",
+          "gradientRed",
+          "gradientOrange"
+        );
+      }
     };
 
     updateBattery();
     // Battery Status Events
-    // batt.addEventListener("chargingchange", () => {
-    //   updateBattery();
-    // });
-    // batt.addEventListener("levelchange", () => {
-    //   updateBattery();
-    // });
+    batt.addEventListener("chargingchange", () => {
+      updateBattery();
+    });
+    batt.addEventListener("levelchange", () => {
+      updateBattery();
+    });
   });
 }
 
