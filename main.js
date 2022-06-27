dc0e9b4272ee7a0097cd381a8b4dfa4f7ef9d7c0
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-27 13:16:24 +0100

    got rid of unnecessary comments. End of project

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function initBattery() {
       if (level == 100) {
         batteryStatus.innerHTML = `Full battery <i class="fa-solid fa-battery-full green "></i>`;
         batteryLiquid.style.height = "103%"; // Hiding the ellipse
-      } else if ((level <= 20) &! batt.charging) {
+      } else if ((level <= 20) & !batt.charging) {
         batteryStatus.innerHTML = `Low battery <i class="fa-solid fa-plug animated-red"></i>`;
       } else if (batt.charging) {
         batteryStatus.innerHTML = `Charging... <i class="fa-solid fa-bolt-lightning greenCharge"></i>`;
@@ -24,17 +24,13 @@ function initBattery() {
       }
       // Changing battery color and removing other colors
       if (level <= 20) {
-     batteryLiquid.classList.add("gradientRed")
- 
+        batteryLiquid.classList.add("gradientRed");
       } else if (level <= 40) {
         batteryLiquid.classList.add("gradientOrange");
-     
       } else if (level <= 80) {
         batteryLiquid.classList.add("gradientYellow");
-        
       } else {
         batteryLiquid.classList.add("gradientGreen");
-       
       }
     };
 
@@ -48,4 +44,3 @@ function initBattery() {
     });
   });
 }
-
